7ab75b3c5edb35f1e6309fad9ce5bacdf415c903
NawfalHadi <nawfalhdi@gmail.com>
2023-05-22 01:46:51 +0800

    gui:add the sidebar

--- program/main.py
+++ program/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit, QSplitter
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 import folium
@@ -14,6 +14,26 @@ class MapWidget(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+        # Create a QSplitter to separate the sidebar and map
+        splitter = QSplitter(self)
+        layout.addWidget(splitter)
+
+        # Create a sidebar widget
+        sidebar_widget = QWidget()
+        sidebar_layout = QVBoxLayout(sidebar_widget)
+
+        # Create a button
+        button = QPushButton("Click Me")
+        sidebar_layout.addWidget(button)
+
+        # Create a text edit for the description
+        description = QTextEdit()
+        description.setPlainText("This is the description sidebar.")
+        sidebar_layout.addWidget(description)
+
+        # Add the sidebar widget to the splitter
+        splitter.addWidget(sidebar_widget)
+
         # Create a Folium map
         map_obj = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
 
@@ -23,7 +43,7 @@ class MapWidget(QWidget):
 
         # Create a web view to display the map
         web_view = QWebEngineView()
-        layout.addWidget(web_view)
+        splitter.addWidget(web_view)
 
         # Load the saved HTML file in the web view
         web_view.load(QUrl.fromLocalFile(map_file))
